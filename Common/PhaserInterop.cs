f08c6f9d5726c74bfca037b2f72e1f2523f342ff
Luis Llamas <luisllamas@hotmail.com>
2023-07-05 17:15:28 +0200

    Add project files.

---
+++ Common/PhaserInterop.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace WpfBlazor.Common
+{
+    public class PhaserInterop
+    {
+        public async Task Init(IJSRuntime js)
+        {            
+            var objectRef = DotNetObjectReference.Create(this);
+            await js.InvokeVoidAsync("init", objectRef);
+        }
+
+        [JSInvokable]
+        public async Task OnPhaserLoaded()
+        {
+        }
+    }
+}
